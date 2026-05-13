d82c73087c99ae4620bce7d6675856f1c0a43664
Dimortaill17 <kruchkovad17@gmail.com>
2026-05-13 18:51:52 +0300

    add deadlines-service

---
+++ backend/deadlines-service/src/main/java/com/studenthub/deadlines/DeadlinesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.studenthub.deadlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadlinesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadlinesServiceApplication.class, args);
+	}
+
+}
---
+++ backend/deadlines-service/src/test/java/com/studenthub/deadlines/DeadlinesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studenthub.deadlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlinesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
