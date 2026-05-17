434eda36115fcba0a176dfa8141c49be9aec3691
dsakunov <d@sakunovv.ru>
2026-05-18 00:12:46 +0300

    update notes-service

---
+++ backend/notes-service/src/main/java/com/studenthub/notes/AuthRequest.java
@@ -0,0 +1,4 @@
+package com.studenthub.notes;
+
+public record AuthRequest(String username, String password) {
+}
---
+++ backend/notes-service/src/main/java/com/studenthub/notes/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.studenthub.notes;
+
+public record AuthResponse(String token) {
+}
